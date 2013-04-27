67569f346a143805ae69dddd2f2257952c3d9a99
Dechhin Lama <ddl2126@columbia.edu>
2013-04-26 22:41:06 -0400

    updated 5/6 input wddl xors

--- wddl_aes/rtl/wddl/xor/wddl_xor5.v
+++ wddl_aes/rtl/wddl/xor/wddl_xor5.v
@@ -1,5 +1,3 @@
-//TODO needs work 
-
 module  wddl_xor5
 (
  d0_p_in
@@ -26,37 +24,34 @@ input   [WIDTH-1 : 0]   d4_p_in, d4_n_in;
 output  [WIDTH-1 : 0]   d_p_out, d_n_out;
 
 wire    [WIDTH-1 : 0]   da_p_w, da_n_w,
-                        db_p_w, db_n_w;
 
 
-wddl_xor2 #(WIDTH)    U_XOR_A
+
+wddl_xor4 #(WIDTH)  4_XOR
 (
  .d0_p_in   ( d0_p_in   )
 ,.d0_n_in   ( d0_n_in   )
 ,.d1_p_in   ( d1_p_in   )
 ,.d1_n_in   ( d1_n_in   )
+,.d2_p_in   ( d2_p_in   )
+,.d2_n_in   ( d2_n_in   )
+,.d3_p_in   ( d3_p_in   )
+,.d3_n_in   ( d3_n_in   )
+
 ,.d_p_out   ( da_p_w    )
 ,.d_n_out   ( da_n_w    )
+
 );
+
 
 wddl_xor2 #(WIDTH)    U_XOR_B
 (
- .d0_p_in   ( d2_p_in   )
-,.d0_n_in   ( d2_n_in   )
-,.d1_p_in   ( d3_p_in   )
-,.d1_n_in   ( d3_n_in   )
-,.d_p_out   ( db_p_w    )
-,.d_n_out   ( db_n_w    )
-);
-
-wddl_xor2 #(WIDTH)    U_XOR
-(
- .d0_p_in   ( da_p_w    )
-,.d0_n_in   ( da_n_w    )
-,.d1_p_in   ( db_p_w    )
-,.d1_n_in   ( db_n_w    )
-,.d_p_out   ( d_p_out   )
-,.d_n_out   ( d_n_out   )
+ .d0_p_in   ( da_p_in   )
+,.d0_n_in   ( da_n_in   )
+,.d1_p_in   ( d4_p_in   )
+,.d1_n_in   ( d4_n_in   )
+,.d_p_out   ( d_p_out    )
+,.d_n_out   ( d_n_out    )
 );
 
 
--- wddl_aes/rtl/wddl/xor/wddl_xor6.v
+++ wddl_aes/rtl/wddl/xor/wddl_xor6.v
@@ -11,6 +11,11 @@ module  wddl_xor6
 ,d2_n_in
 ,d3_p_in
 ,d3_n_in
+,d4_p_in
+,d4_n_in
+,d5_p_in
+,d5_n_in
+
 ,d_p_out
 ,d_n_out
 );
@@ -24,38 +29,36 @@ input   [WIDTH-1 : 0]   d3_p_in, d3_n_in;
 output  [WIDTH-1 : 0]   d_p_out, d_n_out;
 
 wire    [WIDTH-1 : 0]   da_p_w, da_n_w,
-                        db_p_w, db_n_w;
 
 
-wddl_xor2 #(WIDTH)    U_XOR_A
+wddl_xor5 #(WIDTH)  5_XOR
 (
  .d0_p_in   ( d0_p_in   )
 ,.d0_n_in   ( d0_n_in   )
 ,.d1_p_in   ( d1_p_in   )
 ,.d1_n_in   ( d1_n_in   )
+,.d2_p_in   ( d2_p_in   )
+,.d2_n_in   ( d2_n_in   )
+,.d3_p_in   ( d3_p_in   )
+,.d3_n_in   ( d3_n_in   )
+,.d4_p_in   ( d4_p_in   )
+,.d4_n_in   ( d4_n_in   )
+
 ,.d_p_out   ( da_p_w    )
 ,.d_n_out   ( da_n_w    )
+
 );
 
 wddl_xor2 #(WIDTH)    U_XOR_B
 (
- .d0_p_in   ( d2_p_in   )
-,.d0_n_in   ( d2_n_in   )
-,.d1_p_in   ( d3_p_in   )
-,.d1_n_in   ( d3_n_in   )
-,.d_p_out   ( db_p_w    )
-,.d_n_out   ( db_n_w    )
+ .d0_p_in   ( da_p_in   )
+,.d0_n_in   ( da_n_in   )
+,.d1_p_in   ( d5_p_in   )
+,.d1_n_in   ( d5_n_in   )
+,.d_p_out   ( d_p_out    )
+,.d_n_out   ( d_n_out    )
 );
 
-wddl_xor2 #(WIDTH)    U_XOR
-(
- .d0_p_in   ( da_p_w    )
-,.d0_n_in   ( da_n_w    )
-,.d1_p_in   ( db_p_w    )
-,.d1_n_in   ( db_n_w    )
-,.d_p_out   ( d_p_out   )
-,.d_n_out   ( d_n_out   )
-);
 
 
 endmodule
