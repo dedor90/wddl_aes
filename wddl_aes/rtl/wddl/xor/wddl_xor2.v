78d98db0f4c78f638d9c20473da4477c447e1e6b
Dechhin Lama <ddl2126@columbia.edu>
2013-04-26 22:43:13 -0400

    updated 2inputs wddl xor

--- wddl_aes/rtl/wddl/xor/wddl_xor2.v
+++ wddl_aes/rtl/wddl/xor/wddl_xor2.v
@@ -8,10 +8,11 @@ module  wddl_xor2
 ,d_n_out
 );
 
+parameter WIDTH = 1;
 
-input[7:0]   d0_p_in, d0_n_in;
-input[7:0]   d1_p_in, d1_n_in;
-output[7:0]   d_p_out, d_n_out;
+input[WIDTH-1:0]   d0_p_in, d0_n_in;
+input[WIDTH-1:0]   d1_p_in, d1_n_in;
+output[WIDTH-1:0]   d_p_out, d_n_out;
 
 
 assign  d_p_out = (d0_n_in & d1_p_in) | (d0_p_in & d1_n_in);
